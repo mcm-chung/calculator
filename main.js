b518e564c512a5c087c684ad405e520bad064508
May Chung <maychung@mays-mbp.home>
2021-05-17 21:21:17 +0100

    updated calculator progress

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const numbers = document.querySelectorAll(".calculator__buttons__num");
 console.log(numbers);
 const clear = document.querySelector(".calculator__buttons__op__clear");
-const negative = document.querySelector(".calculator__buttons__op__negative");
+// const negative = document.querySelector(".calculator__buttons__op__negative");
 const percentage = document.querySelector(
   "calculator__buttons__op__percentage"
 );
@@ -9,6 +9,7 @@ const divide = document.querySelector(".calculator__buttons__op__divide");
 const multiply = document.querySelector(".calculator__buttons__op__multiply");
 const minus = document.querySelector(".calculator__buttons__op__minus");
 const add = document.querySelector(".calculator__buttons__op__add");
+const equal = document.querySelector(".calculator__buttons__op__equal");
 const display = document.querySelector(".display");
 console.log(display);
 
@@ -16,19 +17,26 @@ console.log(display);
 //   alert;
 //   display.innerHTML = numbers.innerHTML;
 // });
-let addMoreValue = "";
-const updateOutput = (number) => {
-  display.value = number;
-};
+let prevValue = "";
+let operation = null;
+let currentValue = "";
+// const updateOutput = (number) => {
+//   display.value = number;
+// };
 numbers.forEach((number) => {
   number.addEventListener("click", (e) => {
     const storeValue = e.target.innerHTML;
-    addMoreValue = addMoreValue + storeValue;
-    updateOutput(addMoreValue);
+    currentValue = currentValue + storeValue;
+    console.log(currentValue);
+    updateOutput(currentValue);
+    // getTotal();
   });
 });
 
 clear.addEventListener("click", (e) => {
+  currentValue = "";
+  prevValue = "";
+  operation = null;
   updateOutput(0);
 });
 
@@ -38,6 +46,111 @@ clear.addEventListener("click", (e) => {
 
 multiply.addEventListener("click", (e) => {
   const firstValue = display.value;
-  updateOutput(firstValue + "x");
+  operation = "x";
+  prevValue = currentValue;
+  currentValue = "";
+
+  // updateOutput(firstValue + operation );
   console.log(display.value);
+  // getTotal();
+
+  return;
 });
+
+add.addEventListener("click", (e) => {
+  const firstValue = display.value;
+  operation = "+";
+  prevValue = currentValue;
+  currentValue = "";
+
+  // updateOutput(firstValue + operation );
+  console.log(display.value);
+  // getTotal();
+
+  return;
+});
+
+divide.addEventListener("click", (e) => {
+  const firstValue = display.value;
+  operation = "/";
+  prevValue = currentValue;
+  currentValue = "";
+
+  // updateOutput(firstValue + operation );
+  console.log(display.value);
+  // getTotal();
+
+  return;
+});
+
+minus.addEventListener("click", (e) => {
+  const firstValue = display.value;
+  operation = "-";
+  prevValue = currentValue;
+  currentValue = "";
+
+  // updateOutput(firstValue + operation );
+  console.log(display.value);
+  // getTotal();
+
+  return;
+});
+
+const getTotal = () => {
+  if (currentValue === "" || prevValue === "") {
+    return;
+  }
+  const parsecurrentValue = parseFloat(currentValue);
+  const parseprevValue = parseFloat(prevValue);
+  console.log(parsecurrentValue, parseprevValue);
+  let total = 0;
+  // if (operation == "x") {
+  //   total = parsecurrentValue * parseprevValue;
+  //   return total;
+  // } else if (operation == "+") {
+  //   total = parsecurrentValue + parseprevValue;
+  //   return total;
+  // } else if (operation == "/") {
+  //   total = parsecurrentValue / parseprevValue;
+  //   return total;
+  // } else if (operation == "-") {
+  //   total = parsecurrentValue - parseprevValue;
+  //   return total;
+  // }
+  if (operation == "x") {
+    total = parseprevValue * parsecurrentValue;
+    return total;
+  } else if (operation == "+") {
+    total = parseprevValue + parsecurrentValue;
+    return total;
+  } else if (operation == "/") {
+    total = parseprevValue / parsecurrentValue;
+    return total;
+  } else if (operation == "-") {
+    total = parseprevValue - parsecurrentValue;
+    return total;
+  }
+  // else if (operation == "+/-") {
+  //   if parsecurrentValue < 0
+  //     return parse
+  //   total = parsecurrentValue - parseprevValue;
+  //   return total;
+  // }
+  else return NaN;
+
+  // console.log(total);
+  // updateOutput(total);
+  return total;
+};
+
+equal.addEventListener("click", (e) => {
+  const total = getTotal();
+  updateOutput(total);
+  currentValue = total;
+  operator = null;
+  prevValue = "";
+});
+
+const updateOutput = (number) => {
+  display.value = number;
+};
